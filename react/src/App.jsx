650b82d8541d25211c4336f646d4e8180acdec33
Manuel Beltran <manueldbeltranc@gmail.com>
2023-11-24 00:34:02 -0700

    add Header component with simple css

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,7 +1,13 @@
+import Header from './components/Header'
+
 import './App.css'
 
 function App() {
-
+  return(
+    <form >
+      <Header />
+    </form>
+  )
 }
 
 export default App
---
+++ react/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import "../css/Header.css"
+
+export default function Header() {
+  return (
+    <div className="header">
+      <h1>CV Application</h1>
+      <p>Create a new CV with an easy UI</p>
+    </div>
+  )
+}
